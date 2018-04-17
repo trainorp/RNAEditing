d81afb9ba7fe15a69b855f3248bd9ebec3ae421c
trainorp <patrick.trainor@louisville.edu>
2018-04-17 13:07:01 -0400

    idk

--- Logit.py
+++ Logit.py
@@ -4,6 +4,7 @@ import statsmodels.api as sm
 # Fix missing chisq test
 from scipy import stats
 stats.chisqprob = lambda chisq, df: stats.chi2.sf(chisq, df)
+from patsy import dmatrices
 
 # Import data 
 alt = pd.read_csv('/home/patrick/gdrive/RNAEditing/alt_matrix.chr1.tsv',delimiter='\t')
@@ -11,6 +12,7 @@ alt.shape
 alt.dtypes
 ref = pd.read_csv('/home/patrick/gdrive/RNAEditing/ref_matrix.chr1.tsv',delimiter='\t')
 tot = pd.read_csv('/home/patrick/gdrive/RNAEditing/total_matrix.chr1.tsv',delimiter='\t')
+ecpms = pd.read_csv('/home/patrick/gdrive/RNAEditing/ecpms.tsv',delimiter='\t')
 
 # Group indicators:
 g = range(2,23)
@@ -19,36 +21,54 @@ g2 = range(16,23)
 gInd = np.concatenate((np.repeat(1,len(g1)), np.repeat(0,len(g2))))
 
 # One site example:
-def siteTests(alt,ref,tot):
+def siteLogisticTests(alt,ref,tot):
     resDf = pd.DataFrame({'chr': alt['chr'].values, 'pos': alt['pos'].values,
                       'diff': 0.0, 'p': 1.0, 'NT': ''})
     for i in range(alt.shape[0]):
         siteCntDf = pd.DataFrame({'n': tot.iloc[i,g],'Alt': alt.iloc[i,g],
-                                  'Ref': ref.iloc[i,g],'g': gInd})
+                        'Ref': ref.iloc[i,g],'g': gInd, 'ecpms': ecpms['EPCM'].values})
     
-        # Expanded site count DataFrame
+        # Expanded site count DataFrame:
         siteCntExDf = pd.DataFrame(columns=['Alt','g'],dtype=int)
-        
         for j in range(siteCntDf.shape[0]):
+            # Binary indicators for alt vs ref:
             temp1 = np.concatenate((np.repeat(1,siteCntDf['Alt'].values[j]),
                             np.repeat(0,siteCntDf['Ref'].values[j])))
+            # Repeat group indicator and EPCM
             temp2 = np.repeat(siteCntDf['g'].values[j],len(temp1))
-            temp3 = pd.DataFrame(np.stack((temp1,temp2)).T,columns=['Alt','g'])
-            siteCntExDf = siteCntExDf.append(temp3)
+            temp2b = np.repeat(siteCntDf['ecpms'].values[j],len(temp1))
             
+            # Success/Fail & Group:
+            temp3 = pd.DataFrame({'Alt': temp1, 'g': temp2, 'ecpms': temp2b})
+            siteCntExDf = siteCntExDf.append(temp3)
+        
+        # All are in one group only: 
         if sum(siteCntExDf['g']) == siteCntExDf.shape[0]:
-            resDf['NT'].values[i]='a'
+            resDf['NT'].values[i]='All in Ref Group'
             
         elif sum(siteCntExDf['g']) == 0:
-            resDf['NT'].values[i]='b'
+            resDf['NT'].values[i]='All in Comparison Group'
             
+        elif sum(siteCntExDf['Alt']) == siteCntExDf.shape[0]:
+            resDf['NT'].values[i]='All Alt'
+        
+        # Else do chi-square test:
         else:
             # Logistic regression model:
-            logit = sm.Logit(np.array(siteCntExDf.iloc[:,1],dtype=int),
-                             np.array(siteCntExDf.iloc[:,0],dtype=int))
-            logitRes = logit.fit() # Fit model
+            y, X = dmatrices('Alt~ecpms+g',data=siteCntExDf,return_type='dataframe')
+
+            logit = sm.Logit(np.array(siteCntExDf['Alt'].values,dtype=int),X)
+            
+            # Fit model:
+            try:
+                logitRes = logit.fit() # Fit model
+            except np.linalg.LinAlgError as e:
+                resDf['NT'].values[i]=str(e)
             
             # Output:
-            resDf['diff'].values[i] = logitRes.params[0]
-            resDf['p'].values[i] = logitRes.pvalues
-        
+            resDf['diff'].values[i] = logitRes.params['g[T.1]']
+            resDf['p'].values[i] = logitRes.pvalues['g[T.1]']
+            
+    return resDf
+
+siteLogisticTests(alt,ref,tot)
